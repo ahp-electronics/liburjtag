4e7e1f60289b8a82d2f2b4099972e2e68abf2f20
Ilia Platone <info@iliaplatone.com>
2022-12-18 15:02:26 +0100

    Use gettimeofday for cross-compilation

--- src/lib/fclock.c
+++ src/lib/fclock.c
@@ -26,9 +26,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-#ifndef __MINGW32__
-#include <sys/times.h>
-#endif
+#include <sys/time.h>
 #include <math.h>
 #include <assert.h>
 
@@ -36,126 +34,16 @@
 
 /* ------------------------------------------------------------------ */
 
-#ifdef __APPLE__
-#include <mach/mach_time.h>
-#include <sys/types.h>
-#include <sys/time.h>
-
-long double
-urj_lib_frealtime (void)
-{
-    long double result;
-    static uint64_t start_mat;
-    static long double start_time;
-    static double multiplier;
-
-    mach_timebase_info_data_t mtid;
-    struct timeval tv;
-
-    if (!mtid.denom == 0)
-    {
-        mach_timebase_info (&mtid);
-        multiplier = (double) mtid.numer / (double) mtid.denom;
-        gettimeofday (&tv, NULL);
-        start_time =
-            (long double) tv.tv_sec + (long double) tv.tv_usec * 1000.0;
-        start_mat = mach_absolute_time ();
-    }
-
-    result = start_time + (mach_absolute_time () - start_mat) * multiplier;
-
-    assert (isnormal (result));
-    assert (result > 0);
-    return result;
-}
-#else /* def __APPLE__ */
-
-/* ------------------------------------------------------------------ */
-
-#ifdef _POSIX_TIMERS
-
 long double
 urj_lib_frealtime (void)
 {
     long double result;
 
-    struct timezone tz;
-    tz.tz_minuteswest = 0;
-    tz.tz_dsttime = 0;
     struct timeval now;
-    gettimeofday(&now, &tz);
+    gettimeofday(&now, NULL);
     result = (long double) now.tv_sec + now.tv_usec * (long double) 1e-6;
 
     assert (isnormal (result));
     assert (result > 0);
     return result;
 }
-
-#else /* def _POSIX_TIMERS */
-
-/* ------------------------------------------------------------------ */
-
-#ifdef __MINGW32__
-
-#include <sys/timeb.h>
-
-long double
-urj_lib_frealtime (void)
-{
-    long double result;
-
-    struct timeb t;
-
-    ftime (&t);
-    result =
-        (long double) t.time + (long double) t.millitm * (long double) 1e-3;
-
-    assert (isnormal (result));
-    assert (result > 0);
-    return result;
-}
-
-
-#else /* def __MINGW32__ */
-
-/* ------------------------------------------------------------------ */
-
-#ifndef CLK_TCK
-static clock_t CLK_TCK = 0;
-static void set_clk_tck (void) __attribute__ ((constructor));
-static void
-set_clk_tck (void)
-{
-    long v = sysconf (_SC_CLK_TCK);
-    if (v == -1)
-    {
-        perror ("sysconf(_SC_CLK_TCK)");
-        exit (EXIT_FAILURE);
-    }
-    CLK_TCK = v;
-}
-#endif
-
-long double
-urj_lib_frealtime (void)
-{
-    long double result;
-
-
-    struct tms t;
-    clock_t c = times (&t);
-    if (c == (clock_t) - 1)
-    {
-        perror ("urj_lib_frealtime (times)");
-        exit (EXIT_FAILURE);
-    }
-    result = (long double) c / CLK_TCK;
-
-    assert (isnormal (result));
-    assert (result > 0);
-    return result;
-}
-
-#endif
-#endif
-#endif
