b4327be3378c79bc9f8c61345a3b5fdacd7f5073
Ilia Platone <info@iliaplatone.com>
2025-09-12 11:47:30 +0200

    Version 0.14

--- src/global/program.c
+++ src/global/program.c
@@ -30,40 +30,31 @@
 #include <urjtag.h>
 
 
-int32_t program_jtag(int32_t fd, const char *drivername, const char* params[], const char *bsdl_path, int64_t frequency, int32_t device_n)
+int32_t program_jtag(const char *svf_path, const char *drivername, const char *bsdl_path, int64_t frequency)
 {
     int ret = 1, err = 0;
     urj_chain_t *chain;
     urj_part_t *part;
     const urj_cable_driver_t *driver;
-    const urj_param_t **cable_params = NULL;
-    FILE *svf = fdopen(fd, "r");
+    FILE *svf = fopen(svf_path, "r");
     err = (svf == NULL);
     if(err) return ENOENT;
-    if(params != NULL)
-        if(urj_param_init_list (&cable_params, (char**)params, &urj_cable_param_list) != URJ_STATUS_OK)
-            return EINVAL;
     chain = urj_tap_chain_alloc ();
-    if(bsdl_path != NULL)
-        urj_bsdl_set_path (chain, bsdl_path);
-    chain->active_part = device_n;
+    err = (chain == NULL);
+    if(err) return ENOENT;
     if(bsdl_path != NULL)
         urj_bsdl_set_path (chain, bsdl_path);
     driver = urj_tap_cable_find (drivername);
-    int ntries = 5;
-    urj_cable_t *cable = urj_tap_cable_usb_connect (chain, driver, cable_params);
-    while(ntries-- > 0) {
-        urj_tap_cable_set_frequency (cable, frequency);
-        err = urj_tap_detect(chain, 0);
-        if(err) continue;
-        part = urj_tap_chain_active_part (chain);
-        err = (part == NULL);
-        if(err) continue;
-        err = urj_svf_run (chain, svf, 0, frequency);
-        if(err) continue;
-        ret = err;
-        break;
-    }
+    urj_cable_t *cable = urj_tap_cable_usb_connect (chain, driver, NULL);
+    urj_tap_cable_set_frequency (cable, frequency);
+    err = urj_tap_detect(chain, 0);
+    if(err) return ENODEV;
+    part = urj_tap_chain_active_part (chain);
+    err = (part == NULL);
+    if(err) return ENODEV;
+    err = urj_svf_run (chain, svf, 0, frequency);
+    if(err) return ENODEV;
+    ret = err;
     fclose (svf);
     urj_tap_chain_free(chain);
     return ret;
